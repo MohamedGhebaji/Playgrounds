c4f317ea7d3fb0f0685bd042c3425d7dd5c06ed5
Mohamed <ghebaji.mohamed@gmail.com>
2015-04-18 21:45:48 +0100

    Properties

--- Properties.playground/Contents.swift
+++ Properties.playground/Contents.swift
@@ -59,17 +59,65 @@ struct Book {
 }
 
 class Library {
-    var onShelfBooks: [Book]!
-    var onLoanBooks: [Book]!
+    var onShelfBooks: [Book] = []
+    var onLoanBooks: [Book] = []
     
+    var books: [Book] {
+        get {
+            return onLoanBooks + onShelfBooks
+        }
+        set(newValue) {
+            onShelfBooks += newValue
+        }
+    }
     
 }
 
+class OnLineLibrary {
+    var onLoanBooks: [Book] = []
+    var onShelfBooks: [Book] = []
+    lazy var books: [Book] = self.importBooks()
+    
+    
+    func importBooks() -> [Book] {
+        var url = NSURL(string: "http://www.raywenderlich.com/downloads/books.json")
+        var request = NSURLRequest(URL: url!)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+        var error: NSErrorPointer = nil
+        var data: NSData = NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: error)!
+        var json: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+        var importedBooks = [Book]()
+        if let books: [NSDictionary] = json["Books"] as? [NSDictionary] {
+            for book in books {
+                var title: String? = book["Title"] as? String
+                var pageCount: Int? = book["pageCount"] as? Int
+                if title == nil {
+                    title = ""
+                }
+                if pageCount == nil {
+                    pageCount = 0
+                }
+                importedBooks.append(Book(title: title!, pageCount: pageCount!))
+            }
+        }
+        
+        return importedBooks
+    }
+}
 
+var iOSGames = Book(title: "iOS Games by Tuto", pageCount: 819)
+var iOSByTuto = Book(title: "iOS 8 by Tuto", pageCount: 763)
+var iOSAnimation = Book(title: "iOS Animation by Tuto", pageCount: 317)
 
+var library = Library()
+library.onShelfBooks = [iOSGames]
+library.onLoanBooks = [iOSAnimation]
+library.books
+library.books += [iOSByTuto]
+library.onShelfBooks
 
-
-
+var onlineLibrary = OnLineLibrary()
+onlineLibrary.books
 
 
 
